fe085ed7ddf4df332c0fbd03920361d2727d8703
丁梦涛 <15237179193@163.com>
2018-01-31 09:50:51 +0800

    20180131投资人、项目关联表

--- plugins/miniprograms/src/Models/Investor.php
+++ plugins/miniprograms/src/Models/Investor.php
@@ -1,5 +1,6 @@
 <?php namespace WebEd\Plugins\Miniprograms\Models;
 
+use WebEd\Base\Users\Models\User;
 use WebEd\Plugins\Miniprograms\Models\Contracts\InvestorModelContract;
 use WebEd\Base\Models\EloquentBase as BaseModel;
 
@@ -34,4 +35,29 @@ class Investor extends BaseModel implements InvestorModelContract
     const CREATED_AT = 'create_time';
     const UPDATED_AT = 'update_time';
     const DELETED_AT = 'delete_time';
+
+    // 关联微信用户表
+    public function investor_wxtbuser(){
+        return $this->belongsTo(WXTBUser::class,'user_id');
+    }
+    // 关联关注行业
+    public function investor_trades(){
+        return $this->belongsToMany(Trade::class,webed_db_prefix().'investor_trade','investor_id','trade_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function modifier()
+    {
+        return $this->belongsTo(User::class, 'updated_by');
+    }
+
 }
--- plugins/miniprograms/src/Models/Item.php
+++ plugins/miniprograms/src/Models/Item.php
@@ -1,5 +1,6 @@
 <?php namespace WebEd\Plugins\Miniprograms\Models;
 
+use WebEd\Base\Users\Models\User;
 use WebEd\Plugins\Miniprograms\Models\Contracts\ItemModelContract;
 use WebEd\Base\Models\EloquentBase as BaseModel;
 
@@ -33,4 +34,29 @@ class Item extends BaseModel implements ItemModelContract
     const CREATED_AT = 'create_time';
     const UPDATED_AT = 'update_time';
     const DELETED_AT = 'delete_time';
+
+    // 关联所属阶段
+    public function item_stage(){
+        return $this->belongsTo(Stage::class,'stage_id');
+    }
+    // 关联所关注行业
+    public function item_trades(){
+        return $this->belongsToMany(Trade::class,webed_db_prefix().'item_trade','item_id','trade_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function modifier()
+    {
+        return $this->belongsTo(User::class, 'updated_by');
+    }
+
 }
